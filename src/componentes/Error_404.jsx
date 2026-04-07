e9e50016493effbc63b3047676931f74ee67e5a5
Andresito2007 <andreschoqque78@gmail.com>
2026-04-06 21:02:39 -0500

    Update Error_404.jsx

--- src/componentes/Error_404.jsx
+++ src/componentes/Error_404.jsx
@@ -1,1 +1,16 @@
 
+
+import '../estilos/Error_404.css'
+
+function Error_404() {
+  return (
+    <div className="error">
+      <h1 className="mensaje" > Error 404</h1>
+      <p>Pagina no encontrada en Sofa y cafe</p>
+      <a href="/">Volver a Inicio</a>
+    </div>
+  );
+}
+
+
+export default Error_404
